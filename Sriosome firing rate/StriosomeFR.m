7c36672a9f336330d6fd79362ad38104b78bba94
atanu <atanurkm11@gmail.com>
2023-10-20 16:19:48 -0600

    Striosome firing rate

--- Sriosome firing rate/StriosomeFR.m
+++ Sriosome firing rate/StriosomeFR.m
@@ -1,7 +1,8 @@
 % Date: 10/19/2023
 % Striosome firing rate
 
-% load("twdbs.mat");
+% twdbs = load("twdbs.mat");
+twdbData = {twdbs.twdb_control, twdbs.twdb_stress, twdbs.twdb_stress2};
 dbs = {'control', 'stress', 'stress2'}; 
 strs = {'Control', 'Stress', 'Stress2'}; 
 
@@ -9,19 +10,22 @@ BLstart = 60; BLend = 240;
 window_starts = [316,316,321,321,310,310];
 window_ends = [330,330,335,335,320,320];
 
+neuron_types = {'PL Neurons', 'PLS Neurons', 'Striosomes', 'Matrix Neurons', 'HFNs', 'SWNs'};
+
 % Initialize variables to store firing rates and zscores by neuron type
 neuron_type_FRs = cell(1,length(neuron_types));
 
 for neuron_type_idx = 3:3 %1:length(neuron_types)
     % Initialize variables to store firing rates and zscores by database
     neuron_type_FRs{neuron_type_idx} = cell(1,length(dbs));
+    load('cb_ids.mat');
     neuron_type_ids = cb_ids{neuron_type_idx};
     
     for db = 1:length(dbs)
         for neuron_idx = 1:length(neuron_type_ids{db})
             % For each neuron, calculate zscore and firing rate during the
             % active period.
-            [zscore, ~, ~, ~, FR, ~] = quantify_neuron_activity(twdbs{db},...
+            [zscore, ~, ~, ~, FR, ~] = quantify_neuron_activity(twdbData{db},...
                                     neuron_type_ids{db}(neuron_idx),'spikes',...
                                     BLstart,BLend,window_starts(neuron_type_idx),...
                                     window_ends(neuron_type_idx));
@@ -40,6 +44,7 @@ for neuron_type_idx = 3:3 %1:length(neuron_types)
     means = cellfun(@(x) mean(x(~isnan(x))),neuron_type_FRs{neuron_type_idx});
     stderrs = cellfun(@(x) std(x(~isnan(x)))/sqrt(length(x(~isnan(x)))),neuron_type_FRs{neuron_type_idx});
     barwitherr(stderrs,means);
+    strs = {'Control', 'Stress', 'Stress2'};
     set(gca, 'XTickLabel',strs, 'XTick',1:numel(strs));
     
     %Run ttest2
